70309e3531e50c9e1ca7902d5b530f4354b65d0b
Nikhil Sharma <30630904+nik72619c@users.noreply.github.com>
2020-01-14 09:52:33 +0530

    Added logic for Linear Regression (#8)
    * setup victory, added plots for linear regression graph and setup folder structure
    
    * added the logic for linear regression(taking dummy values in mind)

--- src/components/linear-regression/index.js
+++ src/components/linear-regression/index.js
@@ -1,20 +1,81 @@
 import React from "react";
-import { VictoryChart, VictoryScatter, VictoryTheme } from "victory";
+import {
+  VictoryChart,
+  VictoryScatter,
+  VictoryTheme,
+  VictoryLine
+} from "victory";
 
 const data_points = [
-  { x: 3, y: 2 },
-  { x: 4, y: 9 },
-  { x: 5, y: 12 },
-  { x: 7, y: 15 }
+  { x: 1, y: 2 },
+  { x: 7, y: 3 },
+  { x: 3, y: 3 },
+  { x: 7, y: 3 }
 ];
 
 export default class LinearRegression extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      m: 0.64,
+      intercept: 0,
+      learningRate: 0.001,
+      lineDataPoints: []
+    };
+  }
+  applyLinearRegression = () => {
+    let count = 0;
+    let evaluation = 0;
+    let step_size = 0;
+    let newIntercept = this.state.intercept;
+
+    do {
+      console.log("inside do...");
+      data_points.forEach(coordinate => {
+        evaluation +=
+          -2 *
+          (coordinate.y - (this.state.m * coordinate.x + this.state.intercept));
+      });
+      step_size = evaluation * this.state.learningRate;
+      newIntercept = this.state.intercept - step_size;
+      console.log("new intercept", newIntercept);
+      count++;
+      this.setState(
+        {
+          intercept: newIntercept
+        },
+        () => {
+          this.drawLine();
+        }
+      );
+    } while (count !== 100);
+  };
+
+  drawLine = () => {
+    let datapoints = [];
+    for (let i = 0; i < 20; i++) {
+      datapoints.push({
+        x: i,
+        y: this.state.m * i + this.state.intercept
+      });
+    }
+    this.setState({
+      lineDataPoints: datapoints
+    });
+  };
+
   render() {
+    console.log("slope:-", this.state.m);
+    console.log("intercept", this.state.intercept);
     return (
       <div style={{ width: 400, height: 400 }}>
         <h1>Linear regression </h1>
         <div>
-          <VictoryChart theme={VictoryTheme.material}>
+          <VictoryChart
+            // domain={{x: [0, 20], y: [0, 20]}}
+            theme={VictoryTheme.material}
+            animate={{ duration: 2000 }}
+          >
             <VictoryScatter
               style={{
                 data: { stroke: "#c43a31" },
@@ -24,7 +85,17 @@ export default class LinearRegression extends React.Component {
               width={70}
               height={70}
             />
+            <VictoryLine
+              style={{
+                data: { stroke: "#c43a31" },
+                parent: { border: "1px solid #ccc" }
+              }}
+              data={this.state.lineDataPoints}
+            />
           </VictoryChart>
+          <button onClick={this.applyLinearRegression}>
+            apply LinearRegression
+          </button>
         </div>
       </div>
     );
